4fdc5d0bd912ef18e4b5b64ec7eb026952417b8d
Henry Hobhouse <henryhobhouse@zoa.io>
2025-03-08 07:04:43 +0000

    chore: bump dependencies

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -39,21 +39,23 @@ class MyDocument extends Document {
     }
   }
 
-  public render = (): JSX.Element => (
-    <Html lang="en">
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
